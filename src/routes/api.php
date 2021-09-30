8afe38d619d7b535420a06f09b61f968ed519ce0
HusseinAlaa <hussein4alaa@gmail.com>
2021-09-30 14:39:27 +0300

    laravel 8

--- src/routes/api.php
+++ src/routes/api.php
@@ -1,5 +1,14 @@
 <?php
 
+use g4t\Documentation\Controllers\DocumentationController;
 use Illuminate\Support\Facades\Route;
 
+
+$laravel = app();
+$version = $laravel::VERSION;
+if ($version >= 8) {
+    Route::get(config('documentation.json_url'), [DocumentationController::class, 'index']);
+} else {
     Route::get(config('documentation.json_url'), 'DocumentationController@index');
+}
+
--- src/routes/web.php
+++ src/routes/web.php
@@ -2,4 +2,12 @@
 
 use Illuminate\Support\Facades\Route;
 
+
+$laravel = app();
+$version = $laravel::VERSION;
+if ($version >= 8) {
+    Route::get(config('documentation.documentation_url'), [DocumentationController::class, 'web']);
+} else {
     Route::get(config('documentation.documentation_url'), 'DocumentationController@web');
+}
+
